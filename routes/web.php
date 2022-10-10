4cfc04a510ece9a269d5b12b4a714076cae24c61
siam786 <shohrab.hossain36>
2022-10-10 17:10:48 +0600

    problem solving route

--- routes/web.php
+++ routes/web.php
@@ -21,19 +21,25 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/service-page/{service_id?}/{company_name?}', function ($service_id=null,$company_name=null) {
-    return 'Service'.$service_id.''.$company_name;
+Route::get('/service-page/{service_id?}/{company_name?}', function (
+    $service_id = null,
+    $company_name = null
+) {
+    return 'Service' . $service_id . '' . $company_name;
 })->name('service');
 
 Route::get('/contact-us', function () {
     return view('contact');
 })->name('contact');
 
-Route::get('/user/{id}/{name}',function($id,$name){
-   echo  $id,$name;
-})->where(['id'=>'[0-9]+','name','[A-Za-z]+']);
+Route::get('/user/{id}/{name}', function ($id, $name) {
+    echo $id, $name;
+})->where(['id' => '[0-9]+', 'name', '[A-Za-z]+']);
 
-Route::get('/category/{category_name}',function($category_name){
-   
+Route::get('/category/{category_name}', function ($category_name) {
     echo $category_name;
-})->whereIn('category_name',['tv','mobile','laptop']);
+})->whereIn('category_name', ['tv', 'mobile', 'laptop']);
+
+Route::get('/search/{name}',function($name){
+  echo   $name;
+})->where('name','.*');
